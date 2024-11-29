1c7baf4864211edb1dbc51bed507a7a520d02219
CHINTHALA DHEERAJ <dheerajchinthaladev@gmail.com>
2024-11-30 01:35:33 +0530

    Added  Common-Grid Module
    Added  Common-Grid Module

---
+++ src/app/common-grid/common-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common-grid',
+  templateUrl: './common-grid.component.html',
+  styleUrl: './common-grid.component.scss'
+})
+export class CommonGridComponent {
+
+}
